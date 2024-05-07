8f0f71dc0a6551b495e46a0662ff422f60f87d2a
AKMoralesRamos <dental.morak@gmail.com>
2024-05-06 20:50:13 -0600

    componente tips-valor, imagen tip3

--- components/tips-valor.js
+++ components/tips-valor.js
@@ -1,38 +1,67 @@
-import {html, css, LitElement} from 'lit';
+import { html, css, LitElement } from "lit";
 
 export class TipsValor extends LitElement {
-static styles = css`
-.frame{
-    width:100%;
-    height:80px;
-    margin:80px 0;
-    background-color:#266570;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-}
-p{
-    color:#fff;
-    font-size: 26px;
-}
-`;
+  static styles = css`
+    .frame {
+      max-width: 1500px;
+      width: 100%;
+      height: 80px;
+      margin: 80px auto;
+      background-color: #266570;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    p {
+      color: #fff;
+      font-size: 26px;
+    }
+    .tip {
+      width: 10%;
+      height: auto;
+      margin-bottom: 100px;
+    }
+    @media screen and (max-width: 620px) {
+        .frame{
+            height:60px;
+        }
+      p {
+        font-size: 18px;
+      }
+      .tip {
+        width: 15%;
+        margin-bottom: 40px;
+      }
+    }
+  `;
 
-static properties = {
-    message: {type: String}
-};
+  static properties = {
+    message: { type: String },
+    imageFirst: { type: Boolean },
+  };
 
-constructor(){
+  constructor() {
     super();
-}
+    this.imageFirst = false;
+  }
 
-render(){
+  connectedCallback() {
+    super.connectedCallback();
+  }
+
+  render() {
     return html`
-    <div class=frame>
-    <p>${this.message}</p>
-    </div>
+      <div class="frame">
+        ${this.imageFirst
+          ? html`<img class="tip" src="../public/tip2.png" />`
+          : ""}
+        <p>${this.message}</p>
+        ${!this.imageFirst
+          ? html`<img class="tip" src="../public/tip3.png" />`
+          : ""}
+      </div>
     `;
-}
+  }
 }
 
 customElements.define("tips-valor", TipsValor);
-
