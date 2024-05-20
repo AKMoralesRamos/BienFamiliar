e3e7f4f45322e7eaf011c6ea8fa039fa7a5d3bbe
AKMoralesRamos <dental.morak@gmail.com>
2024-05-19 22:53:47 -0600

    componente endo-tx

---
+++ components/endo-tx.js
@@ -0,0 +1,19 @@
+import {html, css, LitElement} from 'lit';
+
+export class EndoTx extends LitElement{
+    static style = css``;
+
+    static properties = {
+
+    };
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return html``;
+    }
+
+}
+customElements.define("endo-tx", EndoTx);
